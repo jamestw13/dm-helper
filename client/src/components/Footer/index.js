8f84ae291f21008c588e966dcf68ea167c296b02
jamestw13 <jamestw13@gmail.com>
2022-11-04 20:24:17 -0500

    fixed package update bugs

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Footer.css';
+const Footer = () => {
+  return (
+    <footer>
+      <div className='footer'>&copy;TJ James 2022</div>
+    </footer>
+  );
+};
+
+export default Footer;
