82af8c20f46bd8610db3e40e2c732eee8ebfd7ea
TJ James <jamestw13@gmail.com>
2023-01-18 13:51:34 -0600

    refactored encounter context and implemented tracker button functionality

--- client/src/components/CharacterRow.jsx
+++ client/src/components/CharacterRow.jsx
@@ -3,37 +3,21 @@ import { Box, Popover, Text, Button, TextInput, Select, NumberInput, Group, Mult
 import { useForm } from '@mantine/form';
 import { EncounterContext } from '../Contexts/EncounterContext';
 import { getTextColor } from '../utils/helpers';
+
 export const CharacterRow = ({ character, roundNum, turnNum }) => {
-  const { characters, setEncounterLog } = useContext(EncounterContext);
+  const { characters } = useContext(EncounterContext);
+  console.log({ characters });
 
   const [formOpen, setFormOpen] = useState(false);
   const noteForm = useForm({
-    initialValues: { condition: '', targets: [], caster: character._id, duration: 1, durationUnit: 'round' },
+    initialValues: { note: '', targets: [], caster: character._id },
   });
 
   const characterValues = characters.map(c => ({ value: c._id, label: c.name }));
 
-  // const handleSubmit = values => {
-  //   setEncounterLog(el => {
-  //     el[roundNum].turns[turnNum].statuses.push(
-  //       ...values.targets.map(t => {
-  //         return {
-  //           condition: values.condition,
-  //           startRound: roundNum,
-  //           startTurn: turnNum,
-  //           target: characters.find(c => c._id === t),
-  //           caster: characters.find(c => c._id === values.caster),
-  //           duration: values.duration,
-  //           durationUnit: values.durationUnit,
-  //         };
-  //       })
-  //     );
-  //     console.log(el);
-
-  //     return el;
-  //   });
-  //   setFormOpen(false);
-  // };
+  const handleSubmit = values => {
+    setFormOpen(false);
+  };
 
   return (
     <>
@@ -54,18 +38,19 @@ export const CharacterRow = ({ character, roundNum, turnNum }) => {
           <Popover.Dropdown>
             <form
               onSubmit={
-                console.log('test')
+                () => {}
+
                 //   noteForm.onSubmit(values => {
                 //   handleSubmit(values);
                 // })
               }
             >
-              <TextInput label="Condition" {...noteForm.getInputProps('condition')} />
-              <Select searchable label="Caster" data={characterValues} {...noteForm.getInputProps('caster')}></Select>
-              <MultiSelect searchable label="Target" data={characterValues} {...noteForm.getInputProps('targets')} />
+              <TextInput label="Condition" {...noteForm.getInputProps('note')} />
+              {/* <Select searchable label="Caster" data={characterValues} {...noteForm.getInputProps('caster')}></Select> */}
+              {/* <MultiSelect searchable label="Target" data={characterValues} {...noteForm.getInputProps('targets')} /> */}
               <Group>
-                <NumberInput label="Duration" {...noteForm.getInputProps('duration')} />
-                <Select data={['turn', 'round']} {...noteForm.getInputProps('durationUnit')} />
+                {/* <NumberInput label="Duration" {...noteForm.getInputProps('duration')} />
+                <Select data={['turn', 'round']} {...noteForm.getInputProps('durationUnit')} /> */}
               </Group>
 
               <Button type="submit">Enter</Button>
@@ -73,10 +58,6 @@ export const CharacterRow = ({ character, roundNum, turnNum }) => {
           </Popover.Dropdown>
         </Popover>
       </td>
-
-      {/* {statuses?.map((status, i) => (
-        <NoteCell key={i} status={status} />
-      ))} */}
     </>
   );
 };
--- client/src/pages/Encounter.jsx
+++ client/src/pages/Encounter.jsx
@@ -6,12 +6,10 @@ import PageWrapper from '../components/PageWrapper';
 import { Section } from '../components/Section';
 
 import { useState, useEffect } from 'react';
-
 import { TrackerNavigator } from '../components/TrackerNavigator';
-import { CharacterRow } from '../components/CharacterRow';
-import { Button, Table } from '@mantine/core';
 import { EncounterContext } from '../Contexts/EncounterContext';
-import NoteCell from '../components/NoteCell';
+import EncounterTable from '../components/EncounterTable';
+import { Affix } from '@mantine/core';
 
 const Encounter = () => {
   const { encounterId } = useParams();
@@ -24,82 +22,26 @@ const Encounter = () => {
   const [currentRound, setCurrentRound] = useState(0);
   const [currentTurn, setCurrentTurn] = useState(0);
 
-  // TODO: replace with mutation
-  const [encounterLog, setEncounterLog] = useState([]);
-
-  // Initialize encounterLog
-
-  const addRound = () => {
-    setNumRounds(x => x + 1);
-  };
-
-  const getRowSpan = effect => {
-    const roundNum = (effect.endRound - effect.startRound) * eData?.characters?.length;
-    const turnOffset = effect.endTurn - effect.startTurn;
-    const result = roundNum - turnOffset;
-    console.log(effect.endRound, effect.startRound, eData.characters.length, roundNum, turnOffset, result);
-    return result;
-  };
-
   return (
-    <EncounterContext.Provider value={{ characters: eData.characters, encounterLog, setEncounterLog }}>
+    <EncounterContext.Provider
+      value={{
+        characters: eData.characters,
+        effects: eData.effects,
+        currentRound,
+        setCurrentRound,
+        currentTurn,
+        setCurrentTurn,
+        numRounds,
+        setNumRounds,
+      }}
+    >
       <PageWrapper title={eData.title}>
         <Section>
-          <TrackerNavigator
-            currentRound={currentRound}
-            setCurrentRound={setCurrentRound}
-            currentTurn={currentTurn}
-            setCurrentTurn={setCurrentTurn}
-          />
-          <Table>
-            <thead>
-              <tr>
-                <th></th>
-                <th>Round</th>
-                <th>Name</th>
-                <th>HP</th>
-                <th>AC</th>
-
-                <th colSpan="100%">Notes</th>
-              </tr>
-            </thead>
-            <tbody id="tracker-table-body">
-              {[...Array(numRounds)].map((round, i) =>
-                eData?.characters?.map((turn, j) => (
-                  <tr key={j}>
-                    <td
-                      style={{
-                        backgroundColor: i === currentRound && j === currentTurn && 'yellow',
-                      }}
-                      onClick={() => {
-                        setCurrentRound(i);
-                        setCurrentTurn(j);
-                      }}
-                    >
-                      &gt;
-                    </td>
-                    {j === 0 && <td rowSpan={eData.characters.length}>{i}</td>}
-                    <CharacterRow character={turn.character} roundNum={i} turnNum={j} />
-                    {eData?.effects?.map(
-                      effect =>
-                        effect.startRound === i &&
-                        effect.startTurn === j && (
-                          <NoteCell key={`${i}+${j}`} effect={effect} rowSpan={getRowSpan(effect)} />
-                        )
-                    )}
-                  </tr>
-                ))
-              )}
-              <tr>
-                <td colSpan="100%">
-                  <Button style={{ width: '100%' }} onClick={addRound}>
-                    Add Round
-                  </Button>
-                </td>
-              </tr>
-            </tbody>
-          </Table>
+          <EncounterTable />
         </Section>
+        <Affix position={{ bottom: '3rem', left: '1rem' }}>
+          <TrackerNavigator />
+        </Affix>
       </PageWrapper>
     </EncounterContext.Provider>
   );
