2771f4b2f597a83e8ddfe02278687924f086d1f3
TJ James <jamestw13@gmail.com>
2023-02-01 23:05:49 -0600

    added userContext

--- client/src/features/users/index.js
+++ client/src/features/users/index.js
@@ -1,6 +1,8 @@
 import Login from './components/Login';
 import Signup from './components/Signup';
 
-import { QUERY_ME, QUERY_USER, QUERY_USERS, LOGIN_USER, ADD_USER } from './services/characterServices';
+import { QUERY_ME, QUERY_USER, QUERY_USERS, LOGIN_USER, ADD_USER } from './services/userServices';
 
-export { Login, Signup, QUERY_ME, QUERY_USER, QUERY_USERS, LOGIN_USER, ADD_USER };
+import { UserContext } from './Contexts/userContext.js';
+
+export { Login, Signup, QUERY_ME, QUERY_USER, QUERY_USERS, LOGIN_USER, ADD_USER, UserContext };
