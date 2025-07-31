19c8ec1db980b622be3637a2b026e0a613113777
TJ James <jamestw13@gmail.com>
2025-07-31 00:49:35 -0500

    Refactoring Signup location

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,9 @@
-import { Title, Text, Stack, Flex, Dialog } from '@mantine/core';
-
 import { PageWrapper, Section } from '../components';
 
-import { Login, Signup, UserContext } from '../features/users';
+import { Login, UserContext } from '../features/users';
 
 import { useContext } from 'react';
 import { Navigate } from 'react-router-dom';
-import { useState } from 'react';
 
 function Home() {
   const { user, loggedIn } = useContext(UserContext);
@@ -16,18 +13,18 @@ function Home() {
   ) : (
     <>
       <PageWrapper title="Stat Block">
-        <Stack align="stretch">
+        <div>
           <Section title="">
             <h1>TTRPG Managment Tools</h1>
-            <Text>A growing suite of tools to help you manage TTRPG games. Includes Encounter Tracking.</Text>
+            <p>A growing suite of tools to help you manage TTRPG games. Includes Encounter Tracking.</p>
           </Section>
 
-          <div style={{ display: 'flex' }} todo justify="space-between">
+          <div style={{ display: 'flex', marginBlock: '.5rem' }} justify="space-between">
             <Section title="Login">
               <Login />
             </Section>
           </div>
-        </Stack>
+        </div>
       </PageWrapper>
     </>
   );
