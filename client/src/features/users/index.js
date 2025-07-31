09332dffa84f3535707f42e06fea1eb38a314b97
TJ James <jamestw13@gmail.com>
2025-07-31 00:49:41 -0500

    Refactoring Signup location

--- client/src/features/users/index.js
+++ client/src/features/users/index.js
@@ -1,8 +1,7 @@
 import Login from './components/Login';
-import Signup from './components/Signup';
 
 import { QUERY_ME, QUERY_USER, QUERY_USERS, LOGIN_USER, ADD_USER } from './services/userServices';
 
 import { UserContext } from './Contexts/userContext.js';
 
-export { Login, Signup, QUERY_ME, QUERY_USER, QUERY_USERS, LOGIN_USER, ADD_USER, UserContext };
+export { Login, QUERY_ME, QUERY_USER, QUERY_USERS, LOGIN_USER, ADD_USER, UserContext };
