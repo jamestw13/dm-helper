13bbe57d65c29e33c196a188a709bddcf97694d2
TJ James <jamestw13@gmail.com>
2025-08-10 14:23:47 -0500

    Removing unused component: Section

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -3,6 +3,5 @@ import Container from './Container';
 import Footer from './Footer';
 import Header from './Header';
 import PageWrapper from './PageWrapper';
-import Section from './Section';
 
-export { Card, Container, Footer, Header, PageWrapper, Section };
+export { Card, Container, Footer, Header, PageWrapper };
