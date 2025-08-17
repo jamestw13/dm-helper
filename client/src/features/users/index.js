474bd5c3f4fcd31db2058c10da92528c5ba758b9
TJ James <jamestw13@gmail.com>
2025-08-17 13:58:18 -0500

    fixed import typo

--- client/src/features/users/index.js
+++ client/src/features/users/index.js
@@ -2,6 +2,6 @@ import Login from './components/Login';
 
 import { QUERY_ME, QUERY_USER, QUERY_USERS, LOGIN_USER, ADD_USER } from './services/userServices';
 
-import { UserContext } from './Contexts/userContext.js';
+import { UserContext } from './contexts/userContext.js';
 
 export { Login, QUERY_ME, QUERY_USER, QUERY_USERS, LOGIN_USER, ADD_USER, UserContext };
